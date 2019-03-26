b8cb364489cfb3f0cf8684b053533b98a1e786f6
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-03-27 01:34:25 +0430

    Deck class implemented(properties + constructor)

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,17 @@
+import java.util.Random;
+
 public class Deck {
+	private Card[] playingCards;
+	private int cardsLeft;
+	private static Random numberGenerator = new Random(123);
+
+	public Deck() {
+		String[] suites = {"spades", "hearts", "diamonds", "clubs"};
+		this.playingCards = new Card[52];
+		for (int i = 0; i < suites.length; i++) {
+			for (int j = 0; j < 13; j++)
+				this.playingCards[i * 13 + j] = new Card(j + 1, suites[j]);
+		}
+		this.cardsLeft = 52;
+	}
 }
