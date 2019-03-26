6b2237e581f8dfa7fff50fb064955daa0fdf2fe7
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-03-27 02:03:09 +0430

    Deck class getCards and getNumOfCards methods implemented

--- src/Deck.java
+++ src/Deck.java
@@ -1,17 +1,27 @@
 import java.util.Random;
 
 public class Deck {
-	private Card[] playingCards;
-	private int cardsLeft;
+	private Card[] cards;
+	private int numOfCards;
 	private static Random numberGenerator = new Random(123);
 
 	public Deck() {
 		String[] suites = {"spades", "hearts", "diamonds", "clubs"};
-		this.playingCards = new Card[52];
+		this.cards = new Card[52];
 		for (int i = 0; i < suites.length; i++) {
 			for (int j = 0; j < 13; j++)
-				this.playingCards[i * 13 + j] = new Card(j + 1, suites[j]);
+				this.cards[i * 13 + j] = new Card(j + 1, suites[i]);
 		}
-		this.cardsLeft = 52;
+		this.numOfCards = 20;
+	}
+
+	public int getNumOfCards() {
+		return this.numOfCards;
+	}
+
+	public Card[] getCards() {
+		Card[] cardsLeft = new Card[this.numOfCards];
+		System.arraycopy(this.cards, 0, cardsLeft, 0, this.numOfCards);
+		return  cardsLeft;
 	}
 }
