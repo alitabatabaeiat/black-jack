eb0d08dfe37305b4a52a52c2a24f4582a499e06f
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2019-03-27 01:21:19 +0430

    Card class implemented

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,26 @@
 public class Card {
+	private int value;
+	private String suit;
+
+	private final String SPADES = "spades";
+	private final String HEARTS = "hearts";
+	private final String DIAMONDS = "diamonds";
+	private final String CLUBS = "clubs";
+
+	public Card(int value, String suit) throws IllegalArgumentException {
+		if (value < 1 || value > 13) throw new IllegalArgumentException("Value is not valid");
+		suit = suit.toLowerCase();
+		if (!(suit.equals(this.SPADES) || suit.equals(this.HEARTS) || suit.equals(this.DIAMONDS) || suit.equals(this.CLUBS)))
+			throw new IllegalArgumentException("Suit is not valid");
+		this.value = value;
+		this.suit = suit.toLowerCase();
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public String getSuit() {
+		return this.suit;
+	}
 }
